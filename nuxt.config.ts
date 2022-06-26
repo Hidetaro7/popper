78c7e434d7ab980aac4d051bce5f3c300eab7974
Hidetaro7 <hidetaro7@gmail.com>
2022-06-26 11:16:24 +0900

    feat: configのテスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/css/app.css"],
+  preset: "node-server",
 });
